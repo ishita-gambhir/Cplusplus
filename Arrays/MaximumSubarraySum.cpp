8691fcd2187420bfc69b3984090e15fb5182d8b8
Ishita Gambhir <39668499+mewcifur28@users.noreply.github.com>
2019-03-30 22:02:13 +0530

    Add files via upload

---
+++ Arrays/MaximumSubarraySum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int maxSubarraySum(int numOfElements, int array[1000]){
+	int i, currentSum = 0, maxSum = 0;
+	for(i = 0; i < numOfElements; i++){
+		currentSum += array[i];
+		if(currentSum < 0){
+			currentSum = 0;
+		}
+		if(currentSum > maxSum){
+			maxSum = currentSum;
+		}
+	}
+	return maxSum;
+}
+
+int main(){
+	int numOfTestCases;
+	cin >> numOfTestCases;
+	for(int t = 1; t <= numOfTestCases; t++){
+		int array[1000], numOfelements;
+	
+		cin >> numOfelements;
+		for(int i = 0; i < numOfelements; i++){
+			cin >> array[i];
+		}
+		cout << maxSubarraySum(numOfelements, array) << endl;
+	}
+	return 0;
+}
