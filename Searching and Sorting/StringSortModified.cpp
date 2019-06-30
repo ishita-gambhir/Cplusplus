eafa4062019a69b0eca2135e10736e37f9388e27
Ishita Gambhir <ishita.gambhir28@gmail.com>
2019-07-01 02:26:21 +0530

    updated modified string sort, still need some changes

--- Searching and Sorting/StringSortModified.cpp
+++ Searching and Sorting/StringSortModified.cpp
@@ -27,16 +27,23 @@ bat
 using namespace std;
 bool myCompare(string s1, string s2){
 	int i = 0;
-	while(s1[i] != '\0' && s1[i] != '\0'){
-		if(s1[i] < s2[i]){
+	//cout << "comparing" << s1 << s2 << endl;
+	while(s1[i] != '\0' && s2[i] != '\0'){
+		if(s1[i] < s2[i]){		
+			//cout << "check1\n";
 			return s1[i] < s2[i];
+		}
+		if(s1[i] > s2[i]){
+			return s1[i] > s2[i];
 		}
 		i++;
 	}
 	if(s1[i] == '\0'){
-		return s2[i] < s1[i];
+		//cout << "Check2\n";
+		return s2[i] > s1[i];
 	}
-	return s1[i] < s2[i];
+	//cout << "check3 \n";
+	return s1[i] > s2[i];
 }
 int main(){
 	int numOfStrings;
