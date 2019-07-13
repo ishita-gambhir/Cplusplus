8848ef9c9cedba4f05de89d057b2459c52ccaec0
Ishita Gambhir <ishita.gambhir28@gmail.com>
2019-07-14 00:06:32 +0530

    added sudoku solver and alt approach for printing substrings

--- Recursion/StringPermutations_approach2.cpp
+++ Recursion/StringPermutations_approach2.cpp
@@ -6,18 +6,16 @@ permutations : abc acb bac bca cab cba */
 #include<string>
 using namespace std;
 //method 2 : swaps elements instead of iterating
-void printAllPermutations(string str, string outputStr = ""){
-	if(str.length() <= 0){
+void printAllPermutations(string str, int index = 0, string outputStr = ""){
+	if(index == str.length()){
 		outputStr.push_back('\0');
 		cout << outputStr << endl;
 		return;
 	}
-	for(int i = 0; str[i] != '\0'; i++){
+	for(int i = index; str[i] != '\0'; i++){
 		swap(str[0], str[i]);
 		outputStr.push_back(str[0]);
-		string remaining = str;
-		remaining.erase(remaining.begin() + 0);
-		printAllPermutations(remaining, outputStr);
+		printAllPermutations(str, index + 1, outputStr);
 		outputStr.pop_back();
 	}
 }
