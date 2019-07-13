06825e8d9a894919e0c36432ccf05655371c8ead
Ishita Gambhir <ishita.gambhir28@gmail.com>
2019-07-14 00:22:40 +0530

    added dict order smaller

--- Recursion/DictionaryOrderSmaller.cpp
+++ Recursion/DictionaryOrderSmaller.cpp
@@ -1,5 +1,5 @@
 /*
-Take as input str, a string. Write a recursive function which prints all the words possible by rearranging the characters of this string which are in dictionary order larger than the given string
+Take as input str, a string. Write a recursive function which returns all the words possible by rearranging the characters of this string which are in dictionary order smaller than the given string.
 
 Input Format:
 Enter a string
@@ -8,19 +8,22 @@ Constraints:
 None
 
 Output Format
-Display all the words larger than the string entered in separate lines
+Display all the words which are in dictionary order smaller than the string entered
 
 Sample Input
 cab
 Sample Output
-cba
+acb
+abc
+bca
+bac
 */
 #include<iostream>
 #include<string>
 using namespace std;
 void printAllPermutations(string original, string str, string outputStr = ""){
 	if(str.length() <= 0){
-		if(outputStr > original && outputStr != original){
+		if(outputStr < original && outputStr != original){
 			outputStr.push_back('\0');
 			cout << outputStr << endl;
 		}
