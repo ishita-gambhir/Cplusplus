6a72f32798f144de2ae3e3c99e7bab00acfe4990
Ishita Gambhir <ishita.gambhir28@gmail.com>
2019-07-14 16:31:04 +0530

    updated reverse stack using recursion

--- Stacks and Queues/ReverseStack_recursion.cpp
+++ Stacks and Queues/ReverseStack_recursion.cpp
@@ -1,3 +1,23 @@
+/*
+Reverse a Stack using Recursion.
+
+Input Format:
+First line contains an integer N (size of the stack). Next N lines follow each containing an integer to be stored in the stack where the last integer is at the top of the stack.
+
+Constraints:
+Output Format
+Output the values of the reversed stack with each value in following line.
+
+Sample Input
+3
+3
+2
+1
+Sample Output
+3
+2
+1
+*/
 #include<iostream>
 #include<stack>
 using namespace std;
@@ -12,10 +32,10 @@ void makeStack(stack<int> &s){
 }
 void printStack(stack<int> s){
 	while(!s.empty()){
-		cout << s.top() << " ";
+		cout << s.top() << endl;
 		s.pop();
 	}
-	cout << endl;
+	//cout << endl;
 }
 void insertAtBottom(stack<int> &s, int data){
 	if(s.empty()){
@@ -39,7 +59,7 @@ void reverseStack(stack<int> &s){
 int main(){
 	stack<int> s;
 	makeStack(s);
-	printStack(s);
+	//printStack(s);
 	reverseStack(s);
 	printStack(s);
 	return 0;
