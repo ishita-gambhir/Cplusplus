39e0891fafc9a6ba0d53ea0b369227b6e335ca2a
Ishita Gambhir <ishita.gambhir28@gmail.com>
2019-08-06 16:15:59 +0530

    updated build and print BT

--- BinaryTrees/PreorderBuildAndPrint.cpp
+++ BinaryTrees/PreorderBuildAndPrint.cpp
@@ -22,16 +22,67 @@ node* buildTree(){
 	root->right = buildTree();
 	return root;
 }
-void printTree(node* root){
+void printTreePreorder(node* root){
+	// root left right
 	if(root == NULL){
 		return;
 	}
 	cout << root->data << " ";
-	printTree(root->left);
-	printTree(root->right);
+	printTreePreorder(root->left);
+	printTreePreorder(root->right);
+}
+void printTreeInorder(node* root){
+	//left root right
+	if(root == NULL){
+		return;
+	}
+	printTreeInorder(root->left);
+	cout << root->data << " ";
+	printTreeInorder(root->right);
+}
+void printTreePostorder(node* root){
+	//left right root
+	if(root == NULL){
+		return;
+	}
+	printTreePostorder(root->left);
+	printTreePostorder(root->right);
+	cout << root->data << " ";
+}
+int heightOfTree(node* root){
+	if(root == NULL){
+		return 0;
+	}
+	int lh = heightOfTree(root->left);
+	int rh = heightOfTree(root->right);
+	return max(lh, rh) + 1;
+}
+void printKthLevel(node* root, int level){
+	if(root == NULL){
+		return;
+	}
+	if(level == 1){
+		cout << root->data << " ";
+		return;
+	}
+	printKthLevel(root->left, level - 1);
+	printKthLevel(root->right, level - 1);
+}
+void printLevelorder(node* root){
+	for(int i = 1; i <= heightOfTree(root); i++){
+		printKthLevel(root, i);
+		cout << endl;
+	}
 }
 int main(){
 	node* tree1 = buildTree();
-	printTree(tree1);
+	printTreePreorder(tree1);
+	cout << endl;
+	printTreeInorder(tree1);
+	cout << endl;
+	printTreePostorder(tree1);
+	cout << endl;
+	printLevelorder(tree1);
+	cout << endl;
 	return 0;
 }
